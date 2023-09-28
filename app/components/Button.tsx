4458a0114b2ef55d3c68fa59b72e93f2f96fc801
RahinF <s3297352@student.rmit.edu.au>
2023-09-28 18:11:37 +1000

    added hero

---
+++ app/components/Button.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+
+interface Props {
+  label: string;
+  iconURL: string;
+  alt: string;
+}
+
+const Button = ({ label, iconURL, alt }: Props) => {
+  return (
+    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+      {label}
+      <Image
+        src={iconURL}
+        alt={alt}
+        className="ml-2 rounded-full h-5 w-5"
+      />
+    </button>
+  );
+};
+
+export default Button;
--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -1,5 +1,66 @@
+import { arrowRight } from '@/app/assets/icons';
+import { bigShoe1 } from '@/app/assets/images';
+import Button from '@/app/components/Button';
+import { shoes, statistics } from '@/app/constants';
+import Image from 'next/image';
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row justify-center min-h-screen flex-col max-container gap-10 p-2"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p className="text-lg font-montserrat text-coral-red">
+          Our Summer Collection
+        </p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <div className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </div>
+
+          <div className="mt-3">
+            <span className="text-coral-red">Nike</span> Shoes
+          </div>
+        </h1>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-8 mb-14 sm:max-w-sm">
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+        <Button
+          label="Shop now"
+          iconURL={arrowRight}
+          alt="arrow right icon"
+        />
+        <div className="flex flex-wrap w-full mt-20 gap-16">
+          {statistics.map((statistic) => (
+            <div key={statistic.label}>
+              <p className="text-4xl font-bold font-palanquin">
+                {statistic.value}
+              </p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {statistic.label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <Image
+          src={bigShoe1}
+          alt="shoe collection"
+          width={610}
+          height={500}
+          className="object-contain z-10"
+        />
+        <div>
+          {shoes.map((shoe, index) => (
+            <div key={index}>shoe {index + 1}</div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
